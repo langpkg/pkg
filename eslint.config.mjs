6cd8463f9f42081e8944f9d15d23f6ffb7afad80
Maysara <Maysara.Elshewehy@gmail.com>
2026-05-20 01:42:50 +0300

    cp

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,64 @@
 
 
 
+// ╔═══════════════════════════════════════ CONST ════════════════════════════════════════╗
+
+    const rules                                   = {
+        "curly"                                   : "off",
+        "@stylistic/semi"                         : ["warn", "always"],
+        "@typescript-eslint/no-empty-function"    : "off",
+        "@typescript-eslint/naming-convention"    : [
+            "warn",
+            {
+                "selector"                        : "variable",
+                "format"                          : ["snake_case", "camelCase"],
+                "filter"                          : {
+                    "match"                       : false,
+                    "regex"                       : "^_"
+                }
+            },
+            {
+                "selector"                        : "function",
+                "format"                          : ["camelCase"]
+            },
+            {
+                "selector"                        : "method",
+                "format"                          : ["camelCase"]
+            },
+            {
+                "selector"                        : "typeLike",
+                "format"                          : ["PascalCase"]
+            },
+            {
+                "selector"                        : "import",
+                "format"                          : ["camelCase", "PascalCase"]
+            },
+            {
+                "selector"                        : "variable",
+                "modifiers"                       : ["const"],
+                "format"                          : ["UPPER_CASE", "snake_case", "camelCase"],
+                "filter"                          : {
+                    "match"                       : false,
+                    "regex"                       : "^_"
+                }
+            },
+            {
+                "selector"                        : "enumMember",
+                "format"                          : ["PascalCase"]
+            }
+        ],
+        "@typescript-eslint/no-unused-vars"       : [
+            "error",
+            {
+                "argsIgnorePattern"               : "^_"
+            }
+        ]
+    };
+
+// ╚══════════════════════════════════════════════════════════════════════════════════════╝
+
+
+
 // ╔════════════════════════════════════════ CONF ════════════════════════════════════════╗
 
     export default defineConfig(
@@ -22,62 +80,9 @@
         js.configs.recommended, ...tseslint.configs.recommended, ...tseslint.configs.stylistic,
 
         {
-            ignores                                       : ['**/.vscode-test', '**/out', 'dist/', 'node_modules/', '.git/'],
-            plugins                                       : { '@stylistic': stylistic },
-
-            rules: {
-
-                "curly"                                   : "off",
-                "@stylistic/semi"                         : ["warn", "always"],
-                "@typescript-eslint/no-empty-function"    : "off",
-                "@typescript-eslint/naming-convention"    : [
-                    "warn",
-                    {
-                        "selector"                            : "variable",
-                        "format"                              : ["snake_case", "camelCase"],
-                        "filter"                              : {
-                            "match"                             : false,
-                            "regex"                             : "^_"
-                        }
-                    },
-                    {
-                        "selector"                            : "function",
-                        "format"                              : ["camelCase"]
-                    },
-                    {
-                        "selector"                            : "method",
-                        "format"                              : ["camelCase"]
-                    },
-                    {
-                        "selector"                            : "typeLike",
-                        "format"                              : ["PascalCase"]
-                    },
-                    {
-                        "selector"                            : "import",
-                        "format"                              : ["camelCase", "PascalCase"]
-                    },
-                    {
-                        "selector"                            : "variable",
-                        "modifiers"                           : ["const"],
-                        "format"                              : ["UPPER_CASE", "snake_case", "camelCase"],
-                        "filter"                              : {
-                            "match"                             : false,
-                            "regex"                             : "^_"
-                        }
-                    },
-                    {
-                        "selector"                            : "enumMember",
-                        "format"                              : ["PascalCase"]
-                    }
-                ],
-                "@typescript-eslint/no-unused-vars"       : [
-                    "error",
-                    {
-                        "argsIgnorePattern"                   : "^_"
-                    }
-                ]
-
-            },
+            ignores     : ['**/.vscode-test', '**/out', 'dist/', 'node_modules/', '.git/'],
+            plugins     : { '@stylistic': stylistic },
+            rules       : rules,
         }
     );
 
--- tsup.config.ts
+++ tsup.config.ts
@@ -12,26 +12,36 @@
 
 
 
-// ╔════════════════════════════════════════ CONF ════════════════════════════════════════╗
+// ╔═══════════════════════════════════════ CONST ════════════════════════════════════════╗
 
-    export default defineConfig({
-
+    const rules        = {
+        "clean"        : true,
+        "dts"          : true,
         "entry"        : ["src/index.ts"],
         "format"       : ["esm", "cjs"],
-        "dts"          : true,
         "minify"       : true,
         "sourcemap"    : false,
-        "clean"        : true,
-        "splitting"    : true,
-        "banner"       : {
-            "js"         : "#!/usr/bin/env bun"
-        },
+        "splitting"    : false,
+        "treeshake"    : true,
+        "target"       : 'es2022',
+        "outDir"       : 'dist',
         "external"     : [
             "bun",
             "@langpkg/mcs_fmt",
             "@langpkg/mcs_gen"
-        ]
+        ],
+        "banner"       : {
+            "js"       : "#!/usr/bin/env bun"
+        }
+    };
 
-    });
+// ╚══════════════════════════════════════════════════════════════════════════════════════╝
+
+
+
+// ╔════════════════════════════════════════ CONF ════════════════════════════════════════╗
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    export default defineConfig(rules as any);
 
 // ╚══════════════════════════════════════════════════════════════════════════════════════╝
